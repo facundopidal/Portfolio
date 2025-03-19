ed7d726af45d20a409f14a5eb54ea2f1a327a50c
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2025-03-19 18:03:20 -0300

    enhance theme switch component with dark mode support and local storage persistence

--- src/app/components/theme-switch/theme-switch.component.ts
+++ src/app/components/theme-switch/theme-switch.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-theme-switch',
@@ -6,13 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './theme-switch.component.html',
   styleUrl: './theme-switch.component.css'
 })
-export class ThemeSwitchComponent {
+export class ThemeSwitchComponent implements OnInit {
+  isDarkTheme: boolean = false;
+
+  ngOnInit(): void {
+    this.isDarkTheme = localStorage.getItem('theme') === 'dark';
+    if (this.isDarkTheme) {
+      document.querySelector('html')?.classList.add('dark');
+    }
+  }
+
   toggleTheme(event: Event) {
     const isChecked = (event.target as HTMLInputElement).checked;
     if (isChecked) {
       document.querySelector('html')?.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
     } else {
       document.querySelector('html')?.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
     }
   }
 }
