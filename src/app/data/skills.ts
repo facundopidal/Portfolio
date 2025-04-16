d8d585a4f8b783f90154c3a49a0bcd805db6c62c
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2025-04-15 22:10:53 -0300

    add skills data and update skills component for improved layout and functionality

---
+++ src/app/data/skills.ts
@@ -0,0 +1,21 @@
+export const skills = {
+    frontend: [
+        {id: "html", name: 'HTML'},
+        {id: "css", name: 'CSS'},
+        {id: "javascript", name: 'JavaScript'},
+        {id: 'tailwind', name: 'Taillwind CSS'},
+        {id: 'typescript', name: 'TypeScript'},
+        {id: "angular", name: 'Angular'},
+    ],
+    backend: [
+        {id: "node", name: 'Node.js'},
+        {id: "express", name: 'Express'},
+        {id: "sqlite", name: 'SQLite'},
+        {id: "mysql", name: 'MySQL'},
+    ],
+    otros: [
+        {id: "git", name: 'Git'},
+        {id: "docker", name: 'Docker'},
+        {id: "tauri", name: 'Tauri'},
+    ],
+}
--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { skills } from '../../data/skills';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  skillsEntries = Object.entries(skills);
 }
