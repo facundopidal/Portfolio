71cbe96688dbab051cf9c600ebfefb69314bbd64
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2025-04-15 22:11:00 -0300

    refactor home component to improve layout and remove unused imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ProjectComponent } from '../../components/project/project.component';
-import { ExperienceComponent } from "../../sections/experience/experience.component";
+import { Component } from '@angular/core';
 import { ProjectsComponent } from "../../sections/projects/projects.component";
 import { HeroComponent } from "../../sections/hero/hero.component";
 import { SkillsComponent } from '../../sections/skills/skills.component';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
-import { ThemeSwitchComponent } from "../../components/theme-switch/theme-switch.component";
+import { AboutMeComponent } from "../../sections/about-me/about-me.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ExperienceComponent, ProjectsComponent, HeroComponent, SkillsComponent, NavBarComponent, ThemeSwitchComponent],
+  imports: [ProjectsComponent, HeroComponent, SkillsComponent, NavBarComponent, AboutMeComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent{
